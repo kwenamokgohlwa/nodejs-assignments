8cc7c848a5f543784993fb5799e9efce14510e06
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-03-28 01:26:21 +0200

    Assignment Basic Node Modules Complete

---
+++ closing.js
@@ -0,0 +1,14 @@
+module.exports = {
+  closingEnglish: function() {
+    console.log("Goodbye");
+  },
+  closingJapanese: function() {
+    console.log("Sayonara");
+  },
+  closingSpanish: function() {
+    console.log("Adiós");
+  },
+  closingIcelandic: function() {
+    console.log("Bless");
+  }
+};
---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const closings = require("./closing.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+closings.closingEnglish();
+
+closings.closingJapanese();
+
+closings.closingSpanish();
+
+closings.closingIcelandic();
